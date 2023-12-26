ea20e07dbc095f86129cfeaaaa89550ea160810c
Andrey Alyamovsky <alyamovskyaa@gmail.com>
2023-12-26 17:03:59 +0300

    recommended_books_parser

--- parser.py
+++ parser.py
@@ -1,6 +1,22 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+
+def parse_recommended_books(url):
+    response = requests.get(url)
+    soup = bs(response.content, 'html.parser')
+    book_list = soup.find_all('div', class_='item')
+    with open("recommended_books.txt", "w", encoding='utf-8') as f:
+        for book in book_list:
+            name = book.find('div', class_='name').contents[0].contents[0]
+            link = book.find('div', class_='name').contents[0]['href']
+            if not book.find('div', class_='author').contents[0].contents:
+                author = ''
+            else:
+                author = book.find('div', class_='author').contents[0].contents[0]
+            f.write(f"{author} - {name} - {'https://flibusta.club' + link}\n")
+
+
 base_url = "https://flibusta.club/"
 response = requests.get(base_url + 'g/')
 soup = bs(response.content, 'html.parser')
@@ -21,5 +37,3 @@ with open("genres.txt", "w", encoding='utf-8') as f:
                     f.write(f"{genre} - {name} - {base_url + link}\n")
                 subgenres_count += 1
         genres_count += 1
-
-
