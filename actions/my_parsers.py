4f2012335e14ec4cc7effe0ee0d6f9a9074e5361
Andrey Alyamovsky <alyamovskyaa@gmail.com>
2023-12-28 02:03:02 +0300

    Update my_parsers.py

--- actions/my_parsers.py
+++ actions/my_parsers.py
@@ -1,20 +1,29 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import random
+
+BOOKS_TO_SHOW = 7
 
 
 def parse_recommended_books(url):
     response = requests.get(url)
     soup = bs(response.content, 'html.parser')
-    book_list = soup.find_all('div', class_='item')
+    books = soup.find_all('div', class_='item')
+    selected_book_numbers = []
+    while len(selected_book_numbers) < BOOKS_TO_SHOW:
+        book_number = random.randint(0, len(books) - 1)
+        if book_number not in selected_book_numbers:
+            selected_book_numbers.append(book_number)
     with open("cache/recommended_books.txt", "w", encoding='utf-8') as f:
-        for book in book_list:
-            title = book.find('div', class_='name').contents[0].contents[0]
-            link = book.find('div', class_='name').contents[0]['href']
-            if not book.find('div', class_='author').contents[0].contents:
-                author = 'авторство не указано'
-            else:
-                author = book.find('div', class_='author').contents[0].contents[0]
-            f.write(f"{title} - {author} - {'https://flibusta.club' + link}\n")
+        for i in range(len(books)):
+            if i in selected_book_numbers:
+                title = books[i].find('div', class_='name').contents[0].contents[0]
+                link = books[i].find('div', class_='name').contents[0]['href']
+                if not books[i].find('div', class_='author').contents[0].contents:
+                    author = 'авторство не указано'
+                else:
+                    author = books[i].find('div', class_='author').contents[0].contents[0]
+                f.write(f"{title} - {author} - {'https://flibusta.club' + link}\n")
 
 
 def book_info(url):
@@ -63,12 +72,20 @@ def search(ask):
                     is_book = is_title = True
                 if is_book:
                     if is_title:
-                        f.write(f"book - {book.contents[0]} - {'https://flibusta.club' + book['href']} - ")
+                        title = ''
+                        for i in range(len(book.contents)):
+                            if i == 1:
+                                title += book.contents[i].contents[0]
+                            else:
+                                title += book.contents[i]
+                        link = 'https://flibusta.club' + book['href']
+                        f.write(f"book - {title} - ")
                     else:
                         if book.contents:
-                            f.write(f"{book.contents[0]}\n")
+                            f.write(f"{book.contents[0]}")
                         else:
-                            f.write('\n')
+                            f.write('авторство не указано')
+                        f.write(f' - {link}\n')
                     is_title = False
                 else:
                     f.write(f"author - {book.contents[0]} - {'https://flibusta.club' + book['href']}\n")
@@ -84,14 +101,15 @@ def books_by_author(url):
                 genres = book.find_all('a', class_="genre_link")
                 genre = ''
                 for line in genres:
-                    genre += line.contents[0] + ' '
+                    if line.contents:
+                        genre += line.contents[0] + ' '
             else:
                 title = book.find('a').contents[0]
                 link = book.find('a')['href']
-                f.write(f'{genre}- {title} - {"https://flibusta.club" + link}\n')
+                f.write(f'{title} - {genre}- {"https://flibusta.club" + link}\n')
 
 
-if __name__ == "__main__":
+def update_genres_and_subgenres():
     base_url = "https://flibusta.club/"
     response = requests.get(base_url + 'g/')
     soup = bs(response.content, 'html.parser')
@@ -110,19 +128,12 @@ if __name__ == "__main__":
                     if subgenres_count % 2 == 1:
                         name = subgenre.contents[0]
                         link = subgenre['href']
-                        f.write(f"{genre} - {name} - {base_url + link}\n")
+                        f.write(f"{genre} - {name} - {'https://flibusta.club' + link}\n")
                     subgenres_count += 1
             genres_count += 1
     with open("cache/genres.txt", "w", encoding='utf-8') as f:
         f.write(genres)
 
 
-# parse_recommended_books(url)
-#
-# with open("recommended_books.txt", "r", encoding='utf-8') as f:
-#     books = f.readlines()
-#     for book in books:
-#         url = book[book.rfind(' - ') + 3:-1]
-#         title, author, genres, rating, rating_count, times_read, description = book_info(url)
-#         print(f'title: {title}, author: {author}\ngenres: {genres}\nrating: {rating}, '
-#               f'rating_count: {rating_count}, times_read: {times_read}\ndescription: {description}')
+if __name__ == "__main__":
+    update_genres_and_subgenres()
